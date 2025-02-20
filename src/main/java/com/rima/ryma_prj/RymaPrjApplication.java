695e480739cd0ef14e3a26447aae01a5b8db63cd
Rima76903 <achourryma971@gmail.com>
2025-02-20 11:43:48 +0100

    first commit

---
+++ src/main/java/com/rima/ryma_prj/RymaPrjApplication.java
@@ -0,0 +1,13 @@
+package com.rima.ryma_prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RymaPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RymaPrjApplication.class, args);
+	}
+
+}
