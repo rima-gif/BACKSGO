6b199827570c485d8fa1dd7532bd270b567b07e3
Rima76903 <achourryma971@gmail.com>
2025-02-20 18:14:25 +0100

    robots

---
+++ src/main/java/com/rima/ryma_prj/infrastructure/controller/RobotController.java
@@ -0,0 +1,13 @@
+package com.rima.ryma_prj.infrastructure.controller;
+
+import com.rima.ryma_prj.application.service.RobotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/robots")
+public class RobotController {
+    @Autowired
+    private RobotService
+}
